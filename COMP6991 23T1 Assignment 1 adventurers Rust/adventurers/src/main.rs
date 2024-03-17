8686b31f6a63fdf35fd24073a408dce59722e7ad
Code Help Tutor <1823890830@qq.com>
2024-03-17 02:23:15 +0000

    WeChat cstutorcs COMP6991

---
+++ COMP6991 23T1 Assignment 1 adventurers Rust/adventurers/src/main.rs
@@ -0,0 +1,8 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+fn main() {
+    println!("Welcome to AdventureRS");
+    println!("To get started, you should read the termgame documentation,");
+    println!("and try out getting a termgame UI to appear on your terminal.");
+}
---
+++ COMP6991 23T1 Assignment 1 adventurers Rust/adventurers_quest/src/lib.rs
@@ -0,0 +1,11 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
